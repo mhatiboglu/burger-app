40fa43bd8c0eb4800961829359d302a49c7697c3
mhatiboglu <mh1741mh@gmail.com>
2020-04-11 16:29:27 +0300

    Add and use  css for layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css'
 
 const layout = (props) => (
     <Aux>
@@ -9,7 +10,7 @@ const layout = (props) => (
             SideDrawer,
             Backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
