02fc6f2939ab4fd694cea859d876028669f06e47
mhatiboglu <mh1741mh@gmail.com>
2020-04-11 23:39:19 +0300

    Create burger and burger ingredients components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
     render() {
         return (
             <Aux>
+                <Burger/>
                 <div>Burger</div>
                 <div>Build Controls</div>
             </Aux>
