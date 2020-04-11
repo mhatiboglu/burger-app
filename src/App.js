6c16d9ec77ac1fdbb2bf56abb189fd60dcd8a46f
mhatiboglu <mh1741mh@gmail.com>
2020-04-11 16:26:08 +0300

    Use burgerbuilder component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
-  render(){
+  render() {
     return (
-        <div>
-          <Layout><p>Test</p></Layout>
-        </div>
-      );
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
- 
+
 }
 
 export default App;
