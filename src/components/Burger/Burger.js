b1fd9883bbd82f5875e4b2014958830a3f20a992
mhatiboglu <mh1741mh@gmail.com>
2020-04-22 13:16:22 +0300

    Add state for burger ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transfromedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i) => {
+                return <BurgerIngredient key={igKey+i} type={igKey} />
+            });
+        });
 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transfromedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,24 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    // constructor(props){
+    //     super(props);
+    //     this.state ={...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+
+    }
     render() {
         return (
             <Aux>
-                <Burger/>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
