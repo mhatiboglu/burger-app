87afcba76baefed569f4eb3067d283521a80aba9
mhatiboglu <mh1741mh@gmail.com>
2020-07-06 17:38:34 +0300

    Handle error with withErrorHandler Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler"
 import axios from "../../axios-orders";
 
 const INGREDIENTS_PRICES = {
@@ -97,7 +98,7 @@ class BurgerBuilder extends Component {
       },
     };
     axios
-      .post("/orders.json", order)
+      .post("/orders", order)
       .then((res) => this.setState({ loading: false, purchasing: false }))
       .catch((err) => this.setState({ loading: false, purchasing: false }));
   };
@@ -153,4 +154,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder,axios);
