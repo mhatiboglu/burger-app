745535310aa3ff245d2959eda3425c43685ffe6f
mhatiboglu <mh1741mh@gmail.com>
2020-07-11 10:33:25 +0300

    Pass ingredients via query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,6 +10,14 @@ class Checkout extends Component {
       meat: 2,
     },
   };
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients });
+  }
   checkoutCanceledHandler = () => {
     this.props.history.goBack();
   };
